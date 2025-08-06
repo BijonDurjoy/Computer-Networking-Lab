bc7e305ff1a86f28e39aa9807cc1ea32fc140c34
BijonDurjoy <bijon176193@gmail.com>
2025-08-06 22:00:12 +0600

    Threading added in the server

--- Socket Programming/server.py
+++ Socket Programming/server.py
@@ -1,15 +1,58 @@
 import socket
+import threading
 
-s= socket.socket()
+#create TCP socket
+server_socket = socket.socket()
 print("Socket created")
 
-s.bind(('localhost', 4444))
-s.listen(2)
-print("Waiting for connections...")
+#Bind to localhost and port 4444
+server_socket.bind(('localhost', 4444))
+server_socket.listen(5)
+print("Server is listening on port 4444...")
 
+#Track Connected Clients
+clients = []
+
+# Brodcast message to all clients
+def broadcast(message, sender_socket= None):
+    for client in clients:
+        if client != sender_socket:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                clients.remove(client)
+
+# Handle each client in a separate thread
+def handle_client(client_socket, address):
+    print(f"New connection from {address}")
+    client_socket.send(b"Welcome to the server!\nEnter your name: ")
+    try:
+        name = client_socket.recv(1024).decode().strip()
+        welcome_msg = f"{name} has joined the chat!\n".encode()
+        broadcast(welcome_msg, client_socket)
+
+        while True:
+            msg= client_socket.recv(1024)
+            if not msg:
+                break
+            full_msg = f"{name}: ".encode() + msg
+            broadcast(full_msg, client_socket)
+        
+    except:
+        pass
+    finally:
+        if client_socket in clients:
+            clients.remove(client_socket)
+        client_socket.close()
+        print(f"{address} disconnected")
+        broadcast(f"{name} left the chat!\n".encode())
+
+
+    
+# Accept clients forever
 while True:
-    c, addr = s.accept()
-    print("Connected with", addr)
-    c.send("Welcome to the server!")
-
-    c.close()
+    client_socket, addr = server_socket.accept()
+    clients.append(client_socket)
+    thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+    thread.start()
